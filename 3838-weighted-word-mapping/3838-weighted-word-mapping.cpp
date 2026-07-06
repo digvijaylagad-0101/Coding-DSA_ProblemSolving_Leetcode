f7fec2c90390e817ba28de3b25ea0320777bb761
digvijaylagad-0101 <digvijay.lagad3108@gmail.com>
2026-07-06 20:36:28 +0530

    Time: 3 ms (27.44%), Space: 43.5 MB (81.18%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
@@ -3,37 +3,20 @@ public:
     string mapWordWeights(vector<string>& words, vector<int>& weights) {
 
         int n = words.size();
-        int cnt = 0;
-        string s = "";
-        map<char,int>mpp;
-
-        for(char ch = 'z';ch >= 'a';ch--) {
-
-            mpp[ch] = cnt;
-            cnt++;
-        }
+        string ans = "";
 
         for(int i=0;i < n;i++) {
-
-            int len = words[i].length();
+            
             int val = 0;
-            for(int j=0;j < len;j++) {
+            for(char ch : words[i]) {
 
-                int pos = words[i][j] - 'a';
+                int pos = ch - 'a';
                 val += weights[pos];
             }
             int rem = val % 26;
-            
-            for(auto it : mpp) {
-
-                if(it.second == rem) {
-
-                    s.push_back(it.first);
-                    break;
-                }
-            }
+            ans += ('z' - rem);
         }
 
-        return s;
+        return ans;
     }
 };
