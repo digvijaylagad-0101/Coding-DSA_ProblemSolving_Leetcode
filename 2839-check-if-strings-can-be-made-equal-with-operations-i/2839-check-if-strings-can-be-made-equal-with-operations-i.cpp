fc25c54a08b89302338137065d7e1bdea9c5571d
digvijaylagad-0101 <digvijay.lagad3108@gmail.com>
2026-07-07 08:56:38 +0530

    Time: 0 ms (100%), Space: 8.1 MB (91.38%) - LeetHub

---
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+
+        int n = s1.size();
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            int next = i + 2;
+
+            if (next < n) {
+
+                if (s1[i] == s2[i] || s1[i] == s2[next]) {
+                    cnt++;
+                }
+            }
+            else {
+
+                if (s1[i] == s2[i] || s1[i] == s2[i - 2]) {
+                    cnt++;
+                }
+            }
+        }
+
+        sort(s1.begin(), s1.end());
+        sort(s2.begin(), s2.end());
+
+        if (s1 == s2 && cnt == n)
+            return true;
+        else
+            return false;
+    }
+};
