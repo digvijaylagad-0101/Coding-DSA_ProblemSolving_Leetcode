6702071db986378bd430ed4b77437db990944379
digvijaylagad-0101 <digvijay.lagad3108@gmail.com>
2026-07-07 14:39:02 +0530

    Time: 0 ms (100%), Space: 8.3 MB (47.28%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -5,28 +5,30 @@ public:
         int n = s1.size();
         int cnt = 0;
 
-        for (int i = 0; i < n; i++) {
+        for(int i=0;i < n;i++) {
 
-            int next = i + 2;
+            int next = i+2;
 
-            if (next < n) {
+            if(next < n) {
 
-                if (s1[i] == s2[i] || s1[i] == s2[next]) {
+                if(s1[i] == s2[next] || s1[i] == s2[i]) {
+
                     cnt++;
                 }
             }
             else {
 
-                if (s1[i] == s2[i] || s1[i] == s2[i - 2]) {
+                if(s1[i] == s2[i-2] || s1[i] == s2[i]) {
+
                     cnt++;
                 }
             }
         }
 
-        sort(s1.begin(), s1.end());
-        sort(s2.begin(), s2.end());
+        sort(s1.begin(),s1.end());
+        sort(s2.begin(),s2.end());
 
-        if (s1 == s2 && cnt == n)
+        if(s1 == s2 && cnt == n)
             return true;
         else
             return false;
