b16f11bb307c760833ee6adb6d51f8e2ae787580
digvijaylagad-0101 <digvijay.lagad3108@gmail.com>
2026-07-09 11:15:15 +0530

    Time: 0 ms (100%), Space: 8 MB (77.47%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+
+public:
+    bool rotateString(string s,string goal) {
+
+        int n = s.length();
+
+        if(n != goal.length()) 
+            return false;
+        
+        for(int i=0;i < n;i++) {
+
+            string ans = s.substr(i) + s.substr(0,i);
+
+            if(ans == goal)
+                
+                return true;
+        }
+
+        return false;
+    }
+};
