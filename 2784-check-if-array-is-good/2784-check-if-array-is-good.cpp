4e53a9367493b738046c81d09414f26bc8bb87eb
digvijaylagad-0101 <digvijay.lagad3108@gmail.com>
2026-07-18 14:21:40 +0530

    Time: 2 ms (20.61%), Space: 27.9 MB (5.89%) - LeetHub

---
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool isGood(vector<int>& nums) {
+        
+        int n = nums.size();
+        map<int,int>mpp;
+
+        for(auto it : nums) {
+
+            mpp[it]++;
+        }
+        
+        int n2 = mpp.size();
+        int cnt = 0,cnt2 = 0,cnt3 = 0;
+
+        for(auto it : mpp) {
+
+            cnt++;
+            if(it.first != cnt)
+               return false;
+
+
+            if(it.second == 1 && cnt != n2) {
+
+                cnt2++;
+            }
+            else if(it.second == 2 && cnt == n2) {
+
+                cnt3++;
+            }
+            else {
+
+                return false;
+            }
+        }
+
+        if((cnt2 == n2-1 || cnt2 == 0) && cnt3 == 1)
+            return true;
+        else 
+            return false;
+    }
+};
