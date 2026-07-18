ab37cf32ad7b16e1f3bd7e7f025e494acd7d224c
digvijaylagad-0101 <digvijay.lagad3108@gmail.com>
2026-07-18 19:29:53 +0530

    Time: 8 ms (11.16%), Space: 30.6 MB (16.9%) - LeetHub

--- 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
@@ -3,28 +3,29 @@ public:
     int minimumDistance(vector<int>& nums) {
 
         int n = nums.size();
-        unordered_map<int, vector<int>> umpp;
+        unordered_map<int,vector<int>> mpp;
         int val3 = INT_MAX;
 
-        for (int i = 0; i < n; i++) {
-            umpp[nums[i]].push_back(i);
+        for(int i=0;i < n;i++) {
+
+            mpp[nums[i]].push_back(i);
         }
 
-        for (auto it : umpp) {
+        for(auto it : mpp) {
 
             int size = it.second.size();
 
-            if (size >= 3) {
+            if(size >= 3) {
 
-                for (int i = 0; i <= size - 3; i++) {
+                for(int i=0;i <= size-3;i++) {
 
                     int val = 0;
 
-                    val += abs(it.second[i] - it.second[i + 1]);
-                    val += abs(it.second[i] - it.second[i + 2]);
-                    val += abs(it.second[i + 1] - it.second[i + 2]);
+                    val += abs(it.second[i] - it.second[i+1]);
+                    val += abs(it.second[i] - it.second[i+2]);
+                    val += abs(it.second[i+1] - it.second[i+2]);
 
-                    val3 = min(val3, val);
+                    val3 = min(val3,val);
                 }
             }
         }
