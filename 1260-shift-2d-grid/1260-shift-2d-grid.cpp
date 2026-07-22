b36fe3b2d8764fb4de554a4425f491e52edaf4dc
digvijaylagad-0101 <digvijay.lagad3108@gmail.com>
2026-07-22 20:02:35 +0530

    Time: 6 ms (22.76%), Space: 20.4 MB (7%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -1,31 +1,37 @@
 class Solution {
+
 public:
-    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+    vector<vector<int>> shiftGrid(vector<vector<int>>&grid,int k) {
 
         int m = grid.size();
         int n = grid[0].size();
-
         vector<int> val;
-        vector<vector<int>> ans(m, vector<int>(n));
+        vector<vector<int>> ans(m,vector<int>(n));
 
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
+        for(int i=0;i < m;i++) {
+
+            for(int j=0;j < n;j++) {
+
                 val.push_back(grid[i][j]);
             }
         }
 
         int size = val.size();
-        k %= size;   // Important
+        k %= size;
 
-        for(int i = 0; i < size - k; i++) {
+        for(int i=0;i < size-k;i++) {
+
             val.push_back(val[i]);
         }
 
-        val.erase(val.begin(), val.begin() + (size - k));
-
+        val.erase(val.begin(),val.begin() + (size-k));
+        
         int idx = 0;
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
+
+        for(int i=0;i < m;i++) {
+
+            for(int j=0;j < n;j++) {
+
                 ans[i][j] = val[idx++];
             }
         }
