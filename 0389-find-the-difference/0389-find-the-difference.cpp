98869fabf1661c346ec89f8f0c33cc226d031146
digvijaylagad-0101 <digvijay.lagad3108@gmail.com>
2026-07-23 00:46:42 +0530

    Time: 1 ms (19.78%), Space: 9 MB (96.2%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        char ch = '\0';
+
+        for(int i=0;i < t.length();i++) {
+
+            if(s[i] != t[i]) {
+
+                ch = t[i];
+                break;
+            }
+        }
+
+        return ch;
+    }
+};
