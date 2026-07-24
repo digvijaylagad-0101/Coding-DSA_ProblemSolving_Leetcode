a6b6aea99d4c397ecefadff8189f717ddc8320d9
digvijaylagad-0101 <digvijay.lagad3108@gmail.com>
2026-07-24 19:30:54 +0530

    Time: 7 ms (18.63%), Space: 9.8 MB (26.08%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        map<char,int> mpp1;
+        map<char,int> mpp2;
+
+        for(auto it : s) {
+
+            mpp1[it]++;
+        }
+        for(auto it : t) {
+
+            mpp2[it]++;
+        }
+
+        if(mpp1 == mpp2)
+           return true;
+        else
+           return false;
+    }
+};
