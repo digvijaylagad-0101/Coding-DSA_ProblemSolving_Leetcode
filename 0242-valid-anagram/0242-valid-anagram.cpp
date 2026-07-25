d4f1722137515d04d954375daafdb20f2820cfb1
digvijaylagad-0101 <digvijay.lagad3108@gmail.com>
2026-07-25 17:18:39 +0530

    Time: 7 ms (18.63%), Space: 9.8 MB (26.15%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,13 +1,31 @@
 class Solution {
-public:
-    bool isAnagram(string s, string t) {
-        
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
 
-        if(s == t)
-           return true;
-        else
-           return false;
+public:
+    bool isAnagram(string s,string t) {
+
+        if(s.length() != t.length())
+            return false;
+        
+        unordered_map<char,int> umpp;
+
+        for(char ch : s) {
+
+            umpp[ch]++;
+        }
+
+        for(char it : t) {
+
+            if(!umpp.count(it))
+                return false;
+            
+            umpp[it]--;
+
+            if(umpp[it] == 0) {
+
+                umpp.erase(it);
+            }
+        }
+
+        return umpp.empty();
     }
 };
