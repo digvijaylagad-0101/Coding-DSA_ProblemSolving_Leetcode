707b8ba4bbb377c90237c27189419b97bf8cd2de
digvijaylagad-0101 <digvijay.lagad3108@gmail.com>
2026-07-27 21:36:29 +0530

    Time: 8 ms (16.27%), Space: 20.7 MB (24.39%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+
+        for(int i=0;i < n-1;i++) {
+
+            if(nums[i] == nums[i+1] && i % 2 != 0) {
+
+                return nums[i-1];
+            }
+        }
+
+        return nums[n-1];
+    }
+};
