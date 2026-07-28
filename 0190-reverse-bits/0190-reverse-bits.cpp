12b39597239a0783b93624a1794e65e82f465224
digvijaylagad-0101 <digvijay.lagad3108@gmail.com>
2026-07-28 23:30:53 +0530

    Time: 0 ms (100%), Space: 9.3 MB (5.37%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    int reverseBits(uint32_t n) {
-
+    int reverseBits(int n) {
+        
         int num = n;
+        int ans = 0;
         string s = "";
 
-        // Store all 32 bits
-        for(int i = 0; i < 32; i++) {
+        for(int i=0;i < 32;i++) {
 
             int val = num % 2;
             s += to_string(val);
             num /= 2;
         }
 
-        int ans = 0;
+        for(int i=0;i < 32;i++) {
 
-        // Convert the string back to decimal
-        for(int i = 0; i < 32; i++) {
-
-            ans += pow(2, 32 - i - 1) * (s[i] - '0');
+            ans += pow(2,32 - i - 1) * (s[i] - '0');
         }
 
         return ans;
