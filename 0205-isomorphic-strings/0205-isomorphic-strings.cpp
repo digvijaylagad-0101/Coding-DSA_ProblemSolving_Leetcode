6e0d44dda0ba91a54079187769517740e3dfc35d
digvijaylagad-0101 <digvijay.lagad3108@gmail.com>
2026-07-31 13:39:39 +0530

    Time: 6 ms (8.52%), Space: 11.7 MB (7.67%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,31 +1,34 @@
 class Solution {
+
 public:
-    bool isIsomorphic(string s, string t) {
+    bool isIsomorphic(string s,string t) {
         
+        if(s.length() != t.length())
+            return false;
+
         unordered_map<char,vector<int>> umpp1;
         unordered_map<char,vector<int>> umpp2;
-        int m = s.length();
-        int n = t.length();
 
-        for(int i=0;i < m;i++) {
+        for(int i=0;i < s.length();i++) {
 
             umpp1[s[i]].push_back(i);
         }
 
-        for(int i=0;i < n;i++) {
+        for(int i=0;i < t.length();i++) {
 
             umpp2[t[i]].push_back(i);
         }
 
-        for(int i=0;i < m;i++) {
+        for(int i=0;i < umpp1.size();i++) {
 
-            if(umpp1[s[i]] != umpp2[t[i]])
-               
-               return false;
-            
-            else 
-               
-               continue;
+            if(umpp1[s[i]] == umpp2[t[i]]) {
+
+                continue;
+            }
+            else {
+
+                return false;
+            }
         }
 
         return true;
