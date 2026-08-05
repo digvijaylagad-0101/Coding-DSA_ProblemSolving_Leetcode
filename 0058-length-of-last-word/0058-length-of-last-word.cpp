e7c1330b46be5b9c4c75c5dccf125cfb48549b15
digvijaylagad-0101 <digvijay.lagad3108@gmail.com>
2026-08-05 21:41:07 +0530

    Time: 0 ms (100%), Space: 8.9 MB (69.34%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        
+        int n = s.length();
+        int cnt = 0;
+
+        for(int i = n-1;i >= 0;i--) {
+
+            if(s[i] != ' ') {
+
+                cnt++;
+            }
+            else if(s[i] == ' ' && cnt > 0) {
+
+                break;
+            }
+            else {
+
+                continue;
+            }
+        }
+
+        return cnt;
+    }
+};
