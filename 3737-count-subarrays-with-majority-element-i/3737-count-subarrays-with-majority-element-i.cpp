5f8a91f43700aa5f5321a943d81b892340b697a7
digvijaylagad-0101 <digvijay.lagad3108@gmail.com>
2026-08-08 19:10:18 +0530

    Time: 43 ms (54.07%), Space: 39.7 MB (50.59%) - LeetHub

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
@@ -4,11 +4,10 @@ public:
 
         int n = nums.size();
         int cnt = 0;
-
+        
         for(int i=0;i < n;i++) {
             
             int val = 0;
-
             for(int j=i;j < n;j++) {
 
                 if(nums[j] == target) {
