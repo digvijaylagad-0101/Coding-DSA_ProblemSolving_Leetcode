4043afa3cf2ee5615b0d2fe85184e0fe8cd326f0
digvijaylagad-0101 <digvijay.lagad3108@gmail.com>
2026-08-15 17:20:43 +0530

    Time: 4 ms (3.35%), Space: 27.8 MB (5.37%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<char> reverseString(vector<char>& s) {
+
+        int n = s.size();
+        char ch = '\0';
+
+        for(int i=0;i < n/2;i++) {
+
+            ch = s[i];
+            s[i] = s[n-i-1];
+            s[n-i-1] = ch;
+        }
+
+        return s;
+    }
+};
