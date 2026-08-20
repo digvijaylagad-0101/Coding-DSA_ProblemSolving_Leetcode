1a45f42142527760c1b2e4ba5b1431134e6c049c
digvijaylagad-0101 <digvijay.lagad3108@gmail.com>
2026-08-20 12:10:50 +0530

    Time: 94 ms (72.96%), Space: 120 MB (59.94%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        
+        int n = candyType.size();
+        int number = n/2;
+        unordered_set<int> s;
+
+        for(int i=0;i < n;i++) {
+
+            s.insert(candyType[i]);
+        }
+        
+        int total = s.size();
+
+        if(number >= total)
+           return total;
+        else 
+           return number;
+    }
+};
