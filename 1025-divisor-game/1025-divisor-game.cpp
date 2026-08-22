497e8b54925f0460778faefd4bc34deba39ac14c
digvijaylagad-0101 <digvijay.lagad3108@gmail.com>
2026-08-22 22:47:00 +0530

    Time: 0 ms (100%), Space: 7.7 MB (57.78%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -2,26 +2,9 @@ class Solution {
 public:
     bool divisorGame(int n) {
         
-        int cnt = 0;
-
-        while(n > 1) {
-
-            int val = n / 2;
-
-            for(int i = 1; i <= val; i++) {
-
-                if(n % i == 0) {
-
-                    n = n - i;
-                    cnt++;
-                    break;
-                }
-            }
-        }
-
-        if(cnt % 2 == 0)
-            return false;
+        if(n % 2 == 0)
+           return true;
         else
-            return true;
+           return false;
     }
 };
