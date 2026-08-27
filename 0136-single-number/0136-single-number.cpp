e473adf9c728c14398a200165068fd4b30d0fee7
digvijaylagad-0101 <digvijay.lagad3108@gmail.com>
2026-08-27 13:53:35 +0530

    Time: 350 ms (5%), Space: 25 MB (7.51%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        
-        int n = nums.size();
-        sort(nums.begin(),nums.end());
 
-        for(int i=0;i < n-1;i++) {
+        unordered_map<int,int> umpp;
+        vector<int> cp = nums;
 
-            if(nums[i] == nums[i+1] && i % 2 != 0) {
+        for(int it : nums) {
 
-                return nums[i-1];
+            umpp[it]++;
+
+            if(umpp[it] == 2) {
+                
+                cp.erase(remove(cp.begin(),cp.end(),it),cp.end());
             }
         }
 
-        return nums[n-1];
+        return cp[0];
     }
 };
