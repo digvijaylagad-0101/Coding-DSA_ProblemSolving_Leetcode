34ccc2c36d3e155ecd298f9e07b21a1a653e4d83
digvijaylagad-0101 <digvijay.lagad3108@gmail.com>
2026-08-27 14:02:05 +0530

    Time: 0 ms (100%), Space: 7.9 MB (50.68%) - LeetHub

---
+++ 3099-harshad-number/3099-harshad-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int sumOfTheDigitsOfHarshadNumber(int x) {
+        
+        int num = x;
+        int sum = 0;
+
+        while(num != 0) {
+
+            int rem = num % 10;
+            sum += rem;
+            num /= 10;
+        }
+
+        if(x % sum == 0)
+           return sum;
+        else
+           return -1;
+    }
+};
