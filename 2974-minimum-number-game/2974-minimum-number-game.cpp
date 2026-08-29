df374e5e52bb5a542dbe12c3a7f46a8207d83755
digvijaylagad-0101 <digvijay.lagad3108@gmail.com>
2026-08-29 12:10:53 +0530

    Time: 0 ms (100%), Space: 28.4 MB (29.09%) - LeetHub

---
+++ 2974-minimum-number-game/2974-minimum-number-game.cpp
@@ -0,0 +1,20 @@
+class Solution {
+
+public:
+    vector<int> numberGame(vector<int>&nums) {
+
+        vector<int> arr;
+        sort(nums.begin(),nums.end());
+        int i=1;
+
+        while(i < nums.size()) {
+
+            arr.push_back(nums[i]);
+            arr.push_back(nums[i-1]);
+
+            i = i + 2;
+        }
+
+        return arr;
+    }
+};
