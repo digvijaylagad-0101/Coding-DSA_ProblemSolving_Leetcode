0277b01666a75a0fd98516e81e95edac08ee2132
digvijaylagad-0101 <digvijay.lagad3108@gmail.com>
2026-09-01 11:34:14 +0530

    Time: 3 ms (14.62%), Space: 20.5 MB (5.67%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+
+        int n = indices.size();
+        map<int,char> mp;
+        string ans = "";
+
+        for(int i=0;i < n;i++) {
+
+            mp[indices[i]] = s[i];
+        }
+
+        for(auto it : mp) {
+
+            ans += it.second;
+        }
+
+        return ans;
+    }
+};
