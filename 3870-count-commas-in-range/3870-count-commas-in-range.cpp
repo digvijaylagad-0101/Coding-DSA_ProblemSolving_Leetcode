920b079f9b6be26f48e9b868b56a079524de6b3d
digvijaylagad-0101 <digvijay.lagad3108@gmail.com>
2026-09-09 17:33:34 +0530

    Time: 0 ms (100%), Space: 8.4 MB (84.7%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -2,26 +2,6 @@ class Solution {
 public:
     int countCommas(int n) {
 
-        if(n < 1000) {
-
-            return 0;
-        }
-        else if(n >= 1000 && n < 10000) {
-
-            int val = n - 1000 + 1;
-            return val;
-        }
-        else if(n >= 10000 && n < 100000) {
-
-            int val = n - 1000 + 1;
-            return val;
-        }
-        else if(n >= 100000) {
-
-            int val = n - 1000 + 1;
-            return val;
-        }
-        
-        return 0;
+        return (n >= 1000) ? n - 1000 + 1 : 0;
     }
 };
