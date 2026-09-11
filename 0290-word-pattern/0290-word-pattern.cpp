c75ba232dac51aab7fbbe408abb39fc1c778e400
digvijaylagad-0101 <digvijay.lagad3108@gmail.com>
2026-09-11 15:52:59 +0530

    Time: 0 ms (100%), Space: 8.8 MB (40.36%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,58 +1,56 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-        
-        int n1 = pattern.length();
-        int n2 = s.length(), cnt = 0;
-        
-        unordered_map<char, vector<int>> mp1;
-        unordered_map<string, vector<int>> mp2;
-        
+
+        int n1 = pattern.length(), n2 = s.length(), cnt = 0;
+        unordered_map<char,vector<int>> mp1;
+        unordered_map<string,vector<int>> mp2;
         string str = "";
 
-        for(int i = 0; i < n1; i++) {
+        for(int i=0;i < n1;i++) {
+
             mp1[pattern[i]].push_back(i);
         }
 
-        for(int i = 0; i < n2; i++) {
+        for(int i=0;i < n2;i++) {
 
             if(s[i] == ' ') {
+
                 if(str != "") {
+
                     mp2[str].push_back(cnt);
                     cnt++;
                     str = "";
                 }
             }
             else {
+
                 str += s[i];
             }
         }
-
-        // Add last word
+        
         if(str != "") {
+
             mp2[str].push_back(cnt);
-            cnt++;
         }
 
-        if(mp1.size() != mp2.size())
-            return false;
-
-        if(cnt != n1)
-            return false;
-
-        for(auto it1 = mp1.begin(); it1 != mp1.end(); it1++) {
+        if(n1 != cnt+1)
+           return false;
+        
+        for(auto it1 = mp1.begin();it1 != mp1.end();it1++) {
             
-            bool found = false;
+            bool flag = false;
 
-            for(auto it2 = mp2.begin(); it2 != mp2.end(); it2++) {
+            for(auto it2 = mp2.begin();it2 != mp2.end();it2++) {
 
                 if(it1->second == it2->second) {
-                    found = true;
+
+                    flag = true;
                     break;
                 }
             }
 
-            if(!found)
+            if(!flag)
                return false;
         }
 
