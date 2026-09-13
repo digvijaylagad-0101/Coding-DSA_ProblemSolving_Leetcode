bac04b484535d8b86de1c6c65ce034285f45748a
digvijaylagad-0101 <digvijay.lagad3108@gmail.com>
2026-09-13 10:23:51 +0530

    Time: 3653 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -2,6 +2,5 @@
 select e2.name as Employee
 from Employee e1
 inner join Employee e2
-on e1.id = e2.managerId    # e2.managerId --> employee detail && e1.id --> manager details
-where e2.salary > e1.salary;
-
+on e1.id = e2.managerId
+where e1.salary < e2.salary;
