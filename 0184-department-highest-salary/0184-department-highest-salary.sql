3a04fbc886ae2e35e9f73071b4b98790457c01fc
digvijaylagad-0101 <digvijay.lagad3108@gmail.com>
2026-09-15 10:20:23 +0530

    Time: 1081 ms (28.76%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,7 +1,8 @@
+# Correlated Subquery : Inner-Query Continuously(for every record)  Dependant on the Outer Query
 select d.name as Department, e.name as Employee, e.salary as Salary
 from Employee as e
 inner join Department as d
 on e.departmentId = d.id
 where e.salary = (select max(e2.salary)
-                  from Employee e2
+                  from Employee as e2
                   where e.departmentId = e2.departmentId);
